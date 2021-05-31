394ef9a8b440a5b0674803497f2ab2f99c498d93
AlexMrgt <git.mrgt@gmail.com>
2021-05-31 21:27:35 +0500

    add preloader

---
+++ src/components/Preloader/Preloader.js
@@ -0,0 +1,17 @@
+const Preloader = () => {
+  return (
+    <div
+      className="preloader"
+    >
+      <div
+        className="preloader__container"
+      >
+        <span
+          className="preloader__round"
+        />
+      </div>
+    </div>
+  )
+};
+
+export default Preloader;
